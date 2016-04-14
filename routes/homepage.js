8f5e564a485a0492a342f076be63b4a0307e2371
Anjelica Thang <amthang@ucsd.edu>
2016-04-13 18:09:07 -0700

    got personal name to show up on the header

--- routes/homepage.js
+++ routes/homepage.js
@@ -1,14 +1,27 @@
+var models = require("../models");
+
 exports.view = function(req, res) {
 
-	models.User.find(username).exec(displayName);
-
-	function displayName(err, data) {
-		if(err) {
-			console.log(err);
-			res.send(500);
-			return;
-		}
-		res.render('homepage', {'welcome_user': data});
+	try {
+		var user = req.session.passport.user;
+	} catch(err) {
+		console.log("no user authenticated");
+		return;
 	}
 
+	// console.log(user.username);
+    //
+	// models.User.find('username': user.username).exec(displayName);
+    //
+	// function displayName(err, name) {
+	// 	if(err) {
+	// 		console.log(err);
+	// 		res.send(500);
+	// 		return;
+	// 	}
+	// 	console.log(name);
+	// 	res.render('homepage', {'welcome_user': name});
+	// }
+
+	res.render('homepage', {'user': user.username});
 };
